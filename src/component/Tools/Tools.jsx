92ac39b8f864f4d95b2701986b4fb6d74e58a9c8
Shivang Sharma <85074821+shivang821@users.noreply.github.com>
2023-06-08 23:28:47 +0530

    adding map method

--- src/component/Tools/Tools.jsx
+++ src/component/Tools/Tools.jsx
@@ -7,11 +7,73 @@ import LockIcon from '@mui/icons-material/Lock';
 import HttpIcon from '@mui/icons-material/Http';
 const Tools = () => {
   const [popupOpen, setPopupOpen] = useState(false)
-  const [toolName,setToolName]=useState("")
+  const [toolName, setToolName] = useState("")
   const openPopup = (tName) => {
     setToolName(tName)
     setPopupOpen(!popupOpen)
   }
+  const data = [
+    {
+      tool_name: 'Ping',
+      desc: 'Get details about a URL',
+      icon: <CellTowerIcon />
+    },
+    {
+      tool_name: "SSL Lookup",
+      desc: "Get details about an SSL Certification",
+      icon: <LockIcon />
+    },
+    {
+      tool_name: "Safe URL Checker",
+      desc: "Check whether a URL is Safe or not",
+      icon: <HttpIcon />
+    },
+    {
+      tool_name: 'Ping',
+      desc: 'Get details about a URL',
+      icon: <CellTowerIcon />
+    },
+    {
+      tool_name: "SSL Lookup",
+      desc: "Get details about an SSL Certification",
+      icon: <LockIcon />
+    },
+    {
+      tool_name: "Safe URL Checker",
+      desc: "Check whether a URL is Safe or not",
+      icon: <HttpIcon />
+    },
+    {
+      tool_name: 'Ping',
+      desc: 'Get details about a URL',
+      icon: <CellTowerIcon />
+    },
+    {
+      tool_name: "SSL Lookup",
+      desc: "Get details about an SSL Certification",
+      icon: <LockIcon />
+    },
+    {
+      tool_name: "Safe URL Checker",
+      desc: "Check whether a URL is Safe or not",
+      icon: <HttpIcon />
+    },
+    {
+      tool_name: 'Ping',
+      desc: 'Get details about a URL',
+      icon: <CellTowerIcon />
+    },
+    {
+      tool_name: "SSL Lookup",
+      desc: "Get details about an SSL Certification",
+      icon: <LockIcon />
+    },
+    {
+      tool_name: "Safe URL Checker",
+      desc: "Check whether a URL is Safe or not",
+      icon: <HttpIcon />
+    },
+  ]
   return (
     <div className='tools' >
       <div className="toolsTop">
@@ -25,164 +87,27 @@ const Tools = () => {
       </div>
       <div className="toolsBottom">
         <div>
-          <div onClick={() => openPopup('Ping')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <CellTowerIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>Ping</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about a url</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('SSL Lookup')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <LockIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>SSL Lookup</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about an SSL Certification</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('Safe URL Checker')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <HttpIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className='toolsItemRight1'>
-                <h3>Safe URL Checker</h3>
-              </div>
-              <div className='toolsItemRight2'>
-                <p>Check whether a URL is Safe or not</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('Ping')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <CellTowerIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>Ping</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about a url</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('SSL Lookup')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <LockIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>SSL Lookup</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about an SSL Certification</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('Safe URL Checker')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <HttpIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className='toolsItemRight1'>
-                <h3>Safe URL Checker</h3>
-              </div>
-              <div className='toolsItemRight2'>
-                <p>Check whether a URL is Safe or not</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('Ping')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <CellTowerIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>Ping</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about a url</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('SSL Lookup')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <LockIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>SSL Lookup</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about an SSL Certification</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('Safe URL Checker')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <HttpIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className='toolsItemRight1'>
-                <h3>Safe URL Checker</h3>
-              </div>
-              <div className='toolsItemRight2'>
-                <p>Check whether a URL is Safe or not</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('Ping')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <CellTowerIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>Ping</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about a url</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('SSL Lookup')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <LockIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className="toolsItemRight1">
-                <h3>SSL Lookup</h3>
-              </div>
-              <div className="toolsItemRight2">
-                <p>Get details about an SSL Certification</p>
-              </div>
-            </div>
-          </div>
-          <div onClick={() => openPopup('Safe URL Checker')} className='toolItem' >
-            <div className='toolItemLeft' >
-              <HttpIcon />
-            </div>
-            <div className='toolItemRight' >
-              <div className='toolsItemRight1'>
-                <h3>Safe URL Checker</h3>
-              </div>
-              <div className='toolsItemRight2'>
-                <p>Check whether a URL is Safe or not</p>
-              </div>
-            </div>
-          </div>
-          
+          {
+            data.map((item) => {
+              return <>
+                <div onClick={() => openPopup(item.tool_name)} className='toolItem' >
+                  <div className='toolItemLeft' >
+                    {item.icon}
+                  </div>
+                  <div className='toolItemRight' >
+                    <div className="toolsItemRight1">
+                      <h3>{item.tool_name}</h3>
+                    </div>
+                    <div className="toolsItemRight2">
+                      <p>{item.desc}</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            })
+          }
         </div>
+
       </div>
       {
         popupOpen && <Popup openPopup={openPopup} name={toolName} />
